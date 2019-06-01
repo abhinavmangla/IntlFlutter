110f5cc66c4f4be126ec820e77e2472b302c8be1
SwiftX4 <mukulkumar627@gmail.com>
2019-06-01 11:55:25 +0530

    Login Page build

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,3 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/main.dart';
-void main() => runApp(MyApp());
+
+
+class loginPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _LoginPageState();
+}
+
+
+class _LoginPageState extends State<loginPage> {
+  @override
+  Widget build(BuildContext context) {
+
+  }
+}
